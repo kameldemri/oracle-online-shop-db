08586ae56e3dc8a680cd129f3c97bf3722a2f301
Demri Kamel <kameldemri00@gmail.com>
2025-07-13 12:28:34 +0100

    improved flow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,24 +9,27 @@ DEV_CONN="$DEV_USER/$DEV_PASS@localhost:1521/$PDB_NAME"
 echo "Starting DB setup..."
 
 echo "Connecting as SYSDBA to perform actions..."
-sqlplus "$SYS_CONN" @"../setup/create_pdb.sql"
-../setup/create_pdb.sql
-
+sqlplus "$SYS_CONN" <<EOF
+@../dba/create_pdb.sql
+@../dba/roles_users.sql
+@../dba/grants.sql
+EXIT;
+EOF
 
 echo "Connecting as $DEV_USER..."
 
-run_sql_as_dev() {
-    echo "Running $1"
-    sqlplus -s "$DEV_CONN" @"$1"
-}
+sqlplus -s "$DEV_CONN" <<EOF
+@../schema/ddl/tables/lookup_tables.sql
+@../schema/dml/lookup_data.sql
+@../schema/ddl/tables/core_tables.sql
+@../schema/ddl/tables/backup_tables.sql
+@../schema/ddl/sequences/sequences.sql
+@../schema/ddl/views/views.sql
+@../schema/plsql/triggers/triggers.sql
+@../schema/plsql/procedures/generate_users.sql
+@../schema/plsql/procedures/generate_products.sql
+@../schema/plsql/procedures/generate_shipments.sql
+EXIT;
+EOF
 
-run_sql_as_dev "../ddl/tables/lookup_tables.sql"
-run_sql_as_dev "../dml/lookup_data.sql"
-run_sql_as_dev "../ddl/tables/core_tables.sql"
-run_sql_as_dev "../ddl/tables/backup_tables.sql"
-run_sql_as_dev "../ddl/sequences/sequences.sql"
-run_sql_as_dev "../ddl/views/views.sql"
-run_sql_as_dev "../plsql/triggers/triggers.sql"
-run_sql_as_dev "../plsql/procedures/generate_users.sql"
-run_sql_as_dev "../plsql/procedures/generate_products.sql"
-run_sql_as_dev "../plsql/procedures/generate_shipments.sql"
+echo "script executed successfully!"
