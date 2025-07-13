182c184450a9789c9d22f7930cbafcc03de14b90
Demri Kamel <kameldemri00@gmail.com>
2025-07-13 12:24:49 +0100

    fix(backup_tables): resolved minor syntax errors

--- schema/ddl/tables/backup_tables.sql
+++ schema/ddl/tables/backup_tables.sql
@@ -14,9 +14,17 @@ CREATE OR REPLACE TRIGGER trg_backup_product
 BEFORE DELETE ON products
 FOR EACH ROW
 BEGIN
-    INSERT INTO products_deleted 
-    SELECT :OLD.*, SYSDATE, NULL;
+    INSERT INTO products_deleted (
+        product_id, name, price, category_id, deleted,
+        created_on, updated_on, created_by, updated_by,
+        deleted_on, deleted_by
+    ) VALUES (
+        :OLD.product_id, :OLD.name, :OLD.price, :OLD.category_id, :OLD.deleted,
+        :OLD.created_on, :OLD.updated_on, :OLD.created_by, :OLD.updated_by,
+        SYSDATE, NULL
+    );
 END;
+/
 
 
 -- SHIPMENTS
@@ -27,9 +35,17 @@ ALTER TABLE shipments_deleted ADD deleted_on DATE DEFAULT SYSDATE;
 ALTER TABLE shipments_deleted ADD deleted_by NUMBER;
 
 CREATE OR REPLACE TRIGGER trg_backup_shipment
-BEFORE DELETE ON shipments 
+BEFORE DELETE ON shipments
 FOR EACH ROW
 BEGIN
-    INSERT INTO shipments_deleted
-    SELECT :OLD.*, SYSDATE, NULL;
+    INSERT INTO shipments_deleted (
+        shipment_id, shipping_date, carrier, max_orders, current_orders, status, deleted,
+        created_on, updated_on, created_by, updated_by,
+        deleted_on, deleted_by
+    ) VALUES (
+        :OLD.shipment_id, :OLD.shipping_date, :OLD.carrier, :OLD.max_orders, :OLD.current_orders, :OLD.status, :OLD.deleted,
+        :OLD.created_on, :OLD.updated_on, :OLD.created_by, :OLD.updated_by,
+        SYSDATE, NULL
+    );
 END;
+/
