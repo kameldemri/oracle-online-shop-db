bf504bbc8cdf06ceb6cb3c54ef0e1e2666eab8c9
Demri Kamel <kameldemri00@gmail.com>
2025-07-13 12:28:08 +0100

    fix(triggers): corrected compilation issues

--- schema/plsql/triggers/triggers.sql
+++ schema/plsql/triggers/triggers.sql
@@ -1,3 +1,4 @@
+
 ---
 -- TRIGGERS
 ---
@@ -9,17 +10,20 @@ FOR EACH ROW
 BEGIN
     :NEW.user_id := seq_user_id.NEXTVAL;
 END;
+/
 
 CREATE OR REPLACE TRIGGER trg_soft_delete_user
 BEFORE DELETE ON users
 FOR EACH ROW
-DECLARE
-    -- Perform soft delete instead
-    UPDATE users SET deleted = 'Y',
+BEGIN
+    UPDATE users
+    SET deleted = 'Y',
         updated_on = SYSDATE
-    -- other cols should be handled using the backend programming lang
     WHERE user_id = :OLD.user_id;
+
+    RAISE_APPLICATION_ERROR(-20001, 'Soft delete performed; original delete aborted.');
 END;
+/
 
 -- CUSTOMERS
 CREATE OR REPLACE TRIGGER trg_create_customer
@@ -37,17 +41,20 @@ BEGIN
         VALUES (:NEW.customer_id);
     END IF;
 END;
+/
 
 CREATE OR REPLACE TRIGGER trg_soft_delete_customer
 BEFORE DELETE ON customers
 FOR EACH ROW
-DECLARE
-    -- Perform soft delete instead
-    UPDATE customers SET deleted = 'Y',
+BEGIN
+    UPDATE customers
+    SET deleted = 'Y',
         updated_on = SYSDATE
-    -- other cols should be handled using the backend programming lang
     WHERE customer_id = :OLD.customer_id;
+
+    RAISE_APPLICATION_ERROR(-20002, 'Soft delete performed; original delete aborted.');
 END;
+/
 
 -- PRODUCTS
 CREATE OR REPLACE TRIGGER trg_product_id
@@ -56,6 +63,7 @@ FOR EACH ROW
 BEGIN
     :NEW.product_id := seq_product_id.NEXTVAL;
 END;
+/
 
 -- ORDERS
 CREATE OR REPLACE TRIGGER trg_order_id
@@ -64,6 +72,7 @@ FOR EACH ROW
 BEGIN
     :NEW.order_id := seq_order_id.NEXTVAL;
 END;
+/
 
 -- SHIPMENTS
 CREATE OR REPLACE TRIGGER trg_shipment_id
@@ -72,17 +81,4 @@ FOR EACH ROW
 BEGIN
     :NEW.shipment_id := seq_shipment_id.NEXTVAL;
 END;
-
--- CUSTOMERS
-CREATE OR REPLACE TRIGGER trg_create_customer
-BEFORE INSERT ON orders
-FOR EACH ROW
-BEGIN
-    -- check if user is already a customer
-    IF NOT EXISTS (
-        SELECT 1 FROM customers WHERE customer_id = :NEW.customer_id
-    ) THEN
-        INSERT INTO customers (customer_id)
-        VALUES (:NEW.customer_id);
-    END IF;
-END;
+/
