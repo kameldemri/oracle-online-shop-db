f237bb8d4c37b40fe210df23b06e6c77125eb245
Demri Kamel <kameldemri00@gmail.com>
2025-07-14 22:48:51 +0100

    improved data generation

--- schema/plsql/procedures/generate_products.sql
+++ schema/plsql/procedures/generate_products.sql
@@ -7,15 +7,14 @@ CREATE OR REPLACE PROCEDURE generate_products (
     TYPE product_rec IS RECORD (
         name         products.name%TYPE,
         price        products.price%TYPE,
-        category_id  products.category_id%TYPE
+        category_id  products.category_id%TYPE,
+        weight       products.weight%TYPE,
+        volume       products.volume%TYPE
     );
-
     l_product_num  NUMBER;
-
     -- Define a table of records type
     TYPE product_tab IS TABLE OF product_rec;
     l_products product_tab := product_tab();  -- init
-
 BEGIN
     FOR i IN 1 .. count_in LOOP
         l_products.EXTEND;
@@ -24,17 +23,21 @@ BEGIN
         l_products(i).name := 'product' || l_product_num;
         l_products(i).price := TRUNC(DBMS_RANDOM.VALUE(100, 1200));
         l_products(i).category_id := TRUNC(DBMS_RANDOM.VALUE(1, 3));
+        l_products(i).weight := TRUNC(DBMS_RANDOM.VALUE(0.1, 10), 2);
+        l_products(i).volume := TRUNC(DBMS_RANDOM.VALUE(0.001, 0.3), 4);
     END LOOP;
-
     -- Bulk insert
     FORALL i IN 1 .. l_products.COUNT
-        INSERT INTO products (name, price, category_id, created_on, created_by)
+        INSERT INTO products (name, price, category_id, created_on, 
+            created_by, weight, volume)
         VALUES (
             l_products(i).name,
             l_products(i).price,
             l_products(i).category_id,
             SYSDATE,
-            created_by_in
+            created_by_in,
+            l_products(i).weight,
+            l_products(i).volume
         );
 END;
 /
