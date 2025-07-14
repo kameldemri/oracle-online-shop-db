8bb19dbd3915d4ef03c16d56cb84e4da2641aee0
Demri Kamel <kameldemri00@gmail.com>
2025-07-14 22:48:20 +0100

    re-implemented the procedure

---
+++ schema/plsql/procedures/generate_orders.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE generate_orders (
+    user_id_in      IN users.user_id%TYPE,
+    total_items_in  IN PLS_INTEGER
+)IS
+    l_order_id orders.order_id%TYPE;
+BEGIN
+    INSERT INTO orders (customer_id, status, created_by) VALUES 
+        (user_id_in, 'pending', user_id_in) RETURNING order_id INTO l_order_id;
+    generate_order_items(l_order_id, total_items_in, user_id_in);
+END;
+/
