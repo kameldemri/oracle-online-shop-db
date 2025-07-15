7c00610cf94ee740f2ea07fcb678b25db42a68c5
Demri Kamel <kameldemri00@gmail.com>
2025-07-15 13:54:44 +0100

    made it DRY

--- schema/plsql/procedures/seed.sql
+++ schema/plsql/procedures/seed.sql
@@ -1,32 +1,13 @@
-BEGIN generate_users(1,1,NULL); END;
-/
-COMMIT;
-
-BEGIN generate_users(100,2,1); END;
-/
-COMMIT;
-
-BEGIN generate_products(100,1); END;
-/
-COMMIT;
-
-BEGIN generate_shipments(20,1); END;
-/
-COMMIT;
-
-BEGIN generate_orders(2, 10); END;
-/
-COMMIT;
-
 BEGIN
+    generate_users(1,1,NULL);
+    generate_users(100,2,1);
+    generate_products(100,1);
+    generate_shipments(20,1);
+    generate_orders(2, 10);
     generate_cities(16, 5);
     generate_cities(35, 5);
-END;
-/
-
-BEGIN
     generate_addresses(2, 2, 2);
     generate_addresses(3, 10, 1);
+    COMMIT;
 END;
 /
-COMMIT;
