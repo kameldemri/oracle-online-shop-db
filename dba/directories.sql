5b6c415ca8dbcc1784ccc43946c95ed4b1097f06
Kamel Demri <kameldemri00@gmail.com>
2025-07-18 23:55:33 +0100

    create seperate directory objects for each backup type and gave grants

--- dba/directories.sql
+++ dba/directories.sql
@@ -1,13 +1,9 @@
--- CDB$ROOT
-CREATE OR REPLACE DIRECTORY dp_backups AS 'C:\oracle_dumps';
-GRANT READ, WRITE ON DIRECTORY dp_backups TO SYS;
+-- CDB
+CREATE OR REPLACE DIRECTORY full_cdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_cdb';
+CREATE OR REPLACE DIRECTORY full_pdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_pdb';
 
 ALTER SESSION SET CONTAINER=ONLINE_SHOP_PDB;
 
-CREATE OR REPLACE DIRECTORY dp_backups AS 'C:\oracle_dumps';
-
-GRANT READ, WRITE ON DIRECTORY dp_backups TO DEVELOPER;
-GRANT READ, WRITE ON DIRECTORY dp_backups TO SUPER_ADMIN;
-
--- Data Pump
-GRANT DATAPUMP_EXP_FULL_DATABASE TO SUPER_ADMIN;
+CREATE OR REPLACE DIRECTORY schema_exp_dir      AS 'C:\oracle_backups\data_pump\schema_exports';
+CREATE OR REPLACE DIRECTORY ts_exp_dir          AS 'C:\oracle_backups\data_pump\tablespaces';
+CREATE OR REPLACE DIRECTORY table_exp_dir       AS 'C:\oracle_backups\data_pump\tables';
