517759e37b351695dfecceea97a0a0433d9babec
Kamel Demri <kameldemri00@gmail.com>
2025-07-20 22:51:43 +0100

    Fix objects creation order

--- dba/directories.sql
+++ dba/directories.sql
@@ -1,9 +1,10 @@
--- CDB
-CREATE OR REPLACE DIRECTORY full_cdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_cdb';
-CREATE OR REPLACE DIRECTORY full_pdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_pdb';
-
-ALTER SESSION SET CONTAINER=ONLINE_SHOP_PDB;
-
+-- PDB
 CREATE OR REPLACE DIRECTORY schema_exp_dir      AS 'C:\oracle_backups\data_pump\schema_exports';
 CREATE OR REPLACE DIRECTORY ts_exp_dir          AS 'C:\oracle_backups\data_pump\tablespaces';
 CREATE OR REPLACE DIRECTORY table_exp_dir       AS 'C:\oracle_backups\data_pump\tables';
+CREATE OR REPLACE DIRECTORY full_pdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_pdb';
+
+-- CDB
+ALTER SESSION SET CONTAINER=CDB$ROOT;
+
+CREATE OR REPLACE DIRECTORY full_cdb_exp_dir    AS 'C:\oracle_backups\data_pump\full_cdb';
